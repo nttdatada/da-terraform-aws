a7c57dd09cc8ba9c7536eed183dd5ca56c5bda56
Marcos Cianci <marcos.lopescianci@nttdata.com>
2022-03-14 20:36:04 -0300

    Refatoring project fault-inject-simulator

--- variables.tf
+++ variables.tf
@@ -18,59 +18,6 @@ variable "profile" {
 }
 
 
-### AWS FIS - AWS CLOUDFORMATION ###
-variable "template" {
-
-    type = map(object({
-        stack_name = string
-        template_body = string
-        parameters = map(string) 
-    }))
-}
-
-
-
-#### AWS RDS ###
-/*
-variable "rds" {
-
-    type = map(object({
-
-        engine                  = string
-        engine_version          = string
-        identifier              = string
-        instance_class          = string
-
-        db_name                    = string
-        username                = string
-        password                = string
-
-        allocated_storage       = number
-        storage_type            = string
-        storage_encrypted       = bool
-        iops                    = number
-
-        backup_retention_period = number
-        backup_window           = string
-        maintenance_window      = string
-
-        multi_az                = bool
-        port                    = number
-        publicly_accessible     = bool
-        
-        vpc_security_group_ids  = list(string)
-    
-
-        apply_immediately       = bool
-
-        monitoring_role_arn     = string
-        monitoring_interval     = number 
-        kms_key_id              = string
-                        
-    }))
-}
-
-*/
-
+### TAGS ###
 
 variable "tags" { }
